6b007afa89f825834f9bce376098f69d28d52523
mnapal <134969649+mnapal@users.noreply.github.com>
2024-10-30 09:53:14 -0300

    Add files via upload

---
+++ dags/etl_yf.py
@@ -0,0 +1,103 @@
+# Este es el DAG que orquesta el ETL Yahoo Finance
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.models import Variable
+from datetime import datetime, timedelta
+from os import environ as env
+from airflow.operators.email import EmailOperator
+
+
+
+
+QUERY_CREATE_TABLE=f"""CREATE TABLE IF NOT EXISTS {env['REDSHIFT_USER']}.{env['REDSHIFT_TABLE']}
+    (openn numeric(10,2) 
+    ,high numeric(10,2)
+    ,low numeric(10,2)
+    ,close  numeric(10,2) 
+    ,date TIMESTAMP WITHOUT TIME ZONE 
+    ,difference numeric(10,2)
+    ,process_date VARCHAR(10) distkey) sortkey(process_date);"""
+
+DELETE_DATE="{{ti.xcom_pull(key=\"process_date\")}}"
+QUERY_CLEAN_PROCESS_DATE = f"""
+DELETE FROM {env['REDSHIFT_USER']}.{env['REDSHIFT_TABLE']} WHERE process_date = \'{DELETE_DATE}\';
+"""
+
+
+def get_process_date(**kwargs):
+    # If process_date is provided take it, otherwise take today
+    if (
+        "process_date" in kwargs["dag_run"].conf
+        and kwargs["dag_run"].conf["process_date"] is not None
+    ):
+        process_date = kwargs["dag_run"].conf["process_date"]
+    else:
+        process_date = kwargs["dag_run"].conf.get(
+            "process_date", datetime.now().strftime("%Y-%m-%d")
+        )
+    kwargs["ti"].xcom_push(key="process_date", value=process_date)
+
+
+default_args = {
+    "owner": "Mauro Napal",
+    "start_date": datetime(2023, 7, 7),
+    "process_date": env['PARAM_PROCESS_DATE'],
+    "retries": int(env['PARAM_RETRIES']) - 1,
+    "retry_delay": timedelta(minutes=1),
+    'email': [env['PARAM_EMAIL_TO_1'],env['PARAM_EMAIL_TO_2']],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'ticker': env['PARAM_TICKER_EMPRESA']
+}
+
+
+with DAG(
+    dag_id="etl_yFinance",
+    default_args=default_args,
+    description="ETL que procesa datos de yFinance",
+    schedule_interval="0 6 * * *",
+    catchup=False,
+) as dag:
+    
+    # Tareas
+    get_process_date_task = PythonOperator(
+        task_id="get_process_date",
+        python_callable=get_process_date,
+        provide_context=True,
+        dag=dag,
+    )
+
+    create_table = SQLExecuteQueryOperator(
+        task_id="create_table",
+        conn_id="redshift_default",
+        sql=QUERY_CREATE_TABLE,
+        dag=dag,
+    )
+
+    clean_process_date = SQLExecuteQueryOperator(
+        task_id="clean_process_date",
+        conn_id="redshift_default",
+        sql=QUERY_CLEAN_PROCESS_DATE,
+        dag=dag,
+    )
+
+    spark_etl_yFinance = SparkSubmitOperator(
+        task_id="spark_etl_yFinance",
+        application=f'{Variable.get("spark_scripts_dir")}/ETL_yFinance.py',
+        conn_id="spark_default",
+        dag=dag,
+        driver_class_path=Variable.get("driver_class_path"),
+    )
+
+    send_email_task = EmailOperator(
+        task_id="send_email",
+        to=default_args['email'],
+        subject="Airflow Alert: Result of ETL execution - {{ds}}",
+        html_content="<p>The Yahoo Finance ETL has ran successfully! </p><p>Stock Ticker: " + default_args['ticker'] + "</p><p>Process date: {{ ti.xcom_pull(key=\"process_date\") }}  </p><p>Host: {{ti.hostname}}</p>",
+        dag=dag,
+    )
+
+    get_process_date_task >> create_table >> clean_process_date >> spark_etl_yFinance >> send_email_task
---
+++ scripts/ETL_yFinance.py
@@ -0,0 +1,88 @@
+from datetime import datetime
+from os import environ as env
+import yfinance as yf
+from pyspark.sql.functions import col, lit
+from pyspark.sql.types import DecimalType
+from commons import ETL_Spark
+
+
+class ETL(ETL_Spark):
+    def __init__(self, job_name=None):
+        super().__init__(job_name)
+
+    def run(self):
+        fecha = env['PARAM_PROCESS_DATE']
+        date_obj = datetime.strptime(fecha, "%Y-%m-%d")
+        self.process_date = date_obj.strftime("%Y-%m-%d")
+        if not fecha:
+            self.process_date = datetime.now().strftime("%Y-%m-%d")
+        self.execute(fecha)
+
+
+    def extract(self):
+        """
+        Extrae datos de la API
+        """
+        print(">>> [E] Extrayendo datos de la API...")
+        ticker = env['PARAM_TICKER_EMPRESA']
+        hist = yf.Ticker(ticker).history(period="1y")
+        hist['Date'] = hist.index
+        data = hist.reset_index(drop=True)
+        if len(data) < 1:
+            raise Exception("Error al obtener datos de Yahoo Finance!")
+        else:
+            print("Muestro datos de la API...")
+            df = self.spark.createDataFrame(data, ['Open', 'High', 'Low', 'Close', 'v', 'd', 's', 'date'])
+            df.printSchema()
+            # selecciono las columnas
+            df = df.select('Open', 'High', 'Low', 'Close', 'date')
+            df.printSchema()
+            df.show()
+            print(">>> [E] Fin Extrayendo datos de la API...")
+            return df
+
+
+    def transform(self, df):
+        """
+        Transforma los datos
+        """
+        print(">>> [T] Transformando datos...")
+        df = df.withColumn("Open", df["Open"].cast(DecimalType(precision=10, scale=2)))
+        df = df.withColumn("High", df["High"].cast(DecimalType(precision=10, scale=2)))
+        df = df.withColumn("Low", df["Low"].cast(DecimalType(precision=10, scale=2)))
+        df = df.withColumn("Close", df["Close"].cast(DecimalType(precision=10, scale=2)))
+        df = df.withColumn('Difference', col('Close') - col('Open'))
+        df = df.withColumn("Difference", df["Difference"].cast(DecimalType(precision=10, scale=2)))
+        df = df.withColumnRenamed("Open", "openn")
+        df = df.withColumn("process_date", lit(self.process_date))
+        print("printSchema")
+        df.printSchema()
+        print("Muestro los valores modificados...")
+        df.show()
+        print(">>> [T] Fin Transformando datos...")
+        return df
+
+
+    def load(self, df_final):
+        """
+        Carga los datos transformados en Redshift
+        """
+        print(">>> [L] Cargando datos en Redshift...")
+        df_final.write \
+            .format("jdbc") \
+            .option("url", env['REDSHIFT_URL']) \
+            .option("dbtable", f"{env['REDSHIFT_SCHEMA']}.{env['REDSHIFT_TABLE']}") \
+            .option("user", env['REDSHIFT_USER']) \
+            .option("password", env['REDSHIFT_PASSWORD']) \
+            .option("driver", "org.postgresql.Driver") \
+            .mode("append") \
+            .save()
+
+        print(">>> [L] Datos cargados exitosamente")
+
+
+if __name__ == "__main__":
+    print("Corriendo script")
+    etl = ETL()
+    etl.run()
+
